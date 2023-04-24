d25e9e8d76b4a166ebff9523d824982f0803f415
Piotr Fijol <piotr.fijol1@gmail.com>
2023-04-24 20:51:51 +0200

    Create Brand component

---
+++ src/components/Brand/brand.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Brand = ({children}) => {
+  return (
+    <div className="brand text-gray-500 hover:opacity-70 hover:cursor-pointer">
+        {children}
+    </div>
+  )
+}
