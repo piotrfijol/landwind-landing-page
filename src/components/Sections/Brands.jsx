211a2373bdeeb1db1f394dac088c0e169c8593b2
Piotr Fijol <piotr.fijol1@gmail.com>
2023-04-24 20:51:12 +0200

    Create Brands section

---
+++ src/components/Sections/Brands.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import Airbnb from "@assets/images/Airbnb.svg";
+import Google from "@assets/images/Google.svg";
+import Mailchimp from "@assets/images/Mailchimp.svg";
+import Microsoft from "@assets/images/Microsoft.svg";
+import Spotify from "@assets/images/Spotify.svg";
+import Uber from "@assets/images/Uber.svg";
+import { Brand } from '../Brand/brand';
+
+export const Brands = () => {
+  return (
+    <div className="container mx-auto grid gap-y-6 grid-cols-2 md:grid-cols-3 xl:grid-cols-6 mb-24 w-full">
+        <Brand>
+            <img src={Google} alt="" srcset="" />
+        </Brand>
+        <Brand>
+            <img src={Microsoft} alt="" />
+        </Brand>           
+        <Brand>
+            <img src={Spotify} alt="" />
+        </Brand>             
+        <Brand>
+            <img src={Mailchimp} alt="" />
+        </Brand>        
+        <Brand>
+            <img src={Airbnb} alt="" />
+        </Brand>        
+        <Brand>
+            <img src={Uber} alt="" />
+        </Brand>  
+    </div>
+  )
+}
