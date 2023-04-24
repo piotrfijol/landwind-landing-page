c4d062bd6a3cff7744640e4183f422450a956de1
Piotr Fijol <piotr.fijol1@gmail.com>
2023-04-24 20:52:08 +0200

    Add white background to navbar navlinks

--- src/components/Navbar/NavLink.jsx
+++ src/components/Navbar/NavLink.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const NavLink = ({to = "#", children}) => {
   return (
-    <li className=" border-gray-200 border-b-2 lg:border-none">
+    <li className="bg-white border-gray-200 border-b-2 lg:border-none">
         <a className="block w-full py-3" href={to} >{children}</a>
     </li>
   )
