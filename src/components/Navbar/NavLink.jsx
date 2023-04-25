d3a54d9b4b475aeef945e3fb9502d21cca0e0cc1
Piotr Fijol <piotr.fijol1@gmail.com>
2023-04-25 21:28:47 +0200

    Fix navbar text styling

--- src/components/Navbar/NavLink.jsx
+++ src/components/Navbar/NavLink.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export const NavLink = ({to = "#", children}) => {
   return (
     <li className="bg-white border-gray-200 border-b-2 lg:border-none">
-        <a className="block w-full py-3" href={to} >{children}</a>
+        <a className="block w-full text-base font-medium" href={to} >{children}</a>
     </li>
   )
 }
--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -10,7 +10,7 @@ export const Navbar = () => {
   return (
     <nav className="py-6 px-3 relative">
         <div className="container flex w-full items-center justify-center gap-8 mx-auto">
-            <div className="flex items-center gap-3 order-first">
+            <div className="flex items-center gap-x-3 order-first">
                 <img className="block" src="/logo.svg" alt="" />
                 <span className="font-semibold text-2xl">Landwind</span>
             </div>
@@ -27,7 +27,7 @@ export const Navbar = () => {
             <div className="ml-auto order-1 lg:order-2">
                 <a className="font-semibold text-sm" href="#">Log In</a>
                 
-                <Button className="ml-3 px-3 py-2 bg-violet-600 hover:bg-violet-800 text-white">Get started</Button>
+                <Button className="ml-3 px-3 py-2 text-sm leading-normal bg-violet-600 hover:bg-violet-800 text-white">Get started</Button>
             </div>
         </div>
     </nav>
