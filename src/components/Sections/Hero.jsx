b1917c9ef2c51ba644459b8e513c3a619c913de2
Piotr Fijol <piotr.fijol1@gmail.com>
2023-04-25 21:30:24 +0200

    Perfect the text-styling and spacing in Hero section

--- src/components/Sections/Hero.jsx
+++ src/components/Sections/Hero.jsx
@@ -5,17 +5,17 @@ import { Subtext } from "@components/Subtext";
 
 export const Hero = () => {
   return (
-    <div className="my-16 flex flex-row gap-x-24 items-center container mx-auto">
-        <div className="py-16">
-            <h1 className="leading-none text-gray-900 font-extrabold text-6xl">Building digital products & brands.</h1>
+    <div className="my-16 flex gap-x-3 flex-row items-center leading-none container mx-auto">
+        <div className="py-16 flex-1">
+            <h1 className="leading-none text-gray-900 font-black text-6xl">Building digital products & brands.</h1>
             <Subtext className="my-6">Here at flowbite we focus on markets where technology, innovation, and capital can unlock long-term value and drive economic growth.</Subtext>
-            <div className="flex gap-4 my-10">
-                <Button className="py-3 px-5 bg-violet-600 hover:bg-violet-800 text-white">Start 30 day free trial</Button>
-                <Button className="py-3 px-5 bg-white text-black border border-gray-200">Pricing & FAQ</Button>
+            <div className="mt-10">
+                <Button className="py-3 px-5 mr-4 text-base font-medium bg-violet-600 hover:bg-violet-800 text-white">Start 30 day free trial</Button>
+                <Button className="py-3 px-5 text-base font-medium bg-white text-black border border-gray-200">Pricing & FAQ</Button>
             </div>
         </div>
         <div className="hidden md:block">
-            <img src={HeroImage} alt="" />
+            <img src={HeroImage}/>
         </div>
     </div>
   )
