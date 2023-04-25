36e02437c9522cb106f2a652b40b4b84d1e4f389
Piotr Fijol <piotr.fijol1@gmail.com>
2023-04-25 21:21:59 +0200

    Update line height of the subtext

--- src/components/Subtext/index.jsx
+++ src/components/Subtext/index.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const Subtext = ({className = "", children}) => {
   return (
-    <p className={`${className} text-gray-500 font-normal text-xl`}>
+    <p className={`${className} text-gray-500 font-normal text-xl leading-normal`}>
         {children}
     </p>
   )
