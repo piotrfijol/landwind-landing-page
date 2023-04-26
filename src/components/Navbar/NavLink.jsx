7c53d2948764d414a1c3040b6d4cad24fc639b50
Piotr Fijol <piotr.fijol1@gmail.com>
2023-04-26 14:45:47 +0200

    Add padding to mobile navlinks

--- src/components/Navbar/NavLink.jsx
+++ src/components/Navbar/NavLink.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const NavLink = ({to = "#", children}) => {
   return (
-    <li className="bg-white border-gray-200 border-b-2 lg:border-none">
+    <li className="bg-white border-gray-200 border-b-2 py-3 lg:py-0 lg:border-none">
         <a className="block w-full text-base font-medium" href={to} >{children}</a>
     </li>
   )
