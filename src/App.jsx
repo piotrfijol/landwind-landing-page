3a50633c6fc4dc2ec8902678767475621a91509c
Piotr Fijol <piotr.fijol1@gmail.com>
2023-04-26 14:47:32 +0200

    Include Features section in the application

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Navbar } from "./components/Navbar";
 import { Hero } from "./components/Sections/Hero";
 import { Brands } from "./components/Sections/Brands";
+import { Features } from "./components/Sections/Features";
 function App() {
 
   return (
@@ -8,6 +9,7 @@ function App() {
       <Navbar />
       <Hero />
       <Brands />
+      <Features />
     </>
   )
 }
