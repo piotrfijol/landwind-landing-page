ca0e69465d3bf039bee1d14252fe57f26948b514
Piotr Fijol <piotr.fijol1@gmail.com>
2023-04-27 14:04:39 +0200

    Export StatCard component

---
+++ src/components/StatCard/index.js
@@ -0,0 +1,12 @@
+import { StatCard } from "./StatCard";
+import { StatCardIcon } from "./StatCardIcon";
+import { StatCardTitle } from "./StatCardTitle";
+import { StatCardDescription } from "./StatCardDescription";
+
+StatCard.Icon = StatCardIcon;
+StatCard.Title = StatCardTitle;
+StatCard.Description = StatCardDescription;
+
+export {
+    StatCard
+};
