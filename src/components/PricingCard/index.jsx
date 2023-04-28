17b349457b461f6218ed58e86b5b4849a376ac61
Piotr Fijol <piotr.fijol1@gmail.com>
2023-04-28 15:07:44 +0200

    Create PricingCard component

---
+++ src/components/PricingCard/index.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { Subtext } from "../Subtext";
+import { Card } from "flowbite-react";
+
+export const PricingCard = ({className = "", plan, description, cost, costPer, benefits}) => {
+  return (
+    <Card className={`${className} p-2 text-center max-w-sm lg:max-w-none`}>
+      <h2 className='text-2xl font-semibold'>
+        {plan}
+      </h2>
+      <Subtext className="!text-lg pb-4">{description}</Subtext>
+      <div className="mx-auto mb-4 flex items-baseline text-gray-900 dark:text-white">
+        <span className="text-5xl font-extrabold tracking-tight">
+          {cost}
+        </span>
+        <span className="ml-2.5 text-lg font-normal text-gray-500 dark:text-gray-400">
+          /{costPer}
+        </span>
+      </div>
+      { benefits }
+    <button
+      type="button"
+      className="mt-auto w-full justify-center rounded-lg bg-purple-600 0 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-purple-500 focus:outline-none focus:ring-4 focus:ring-blue-200"
+    >
+      Get started
+    </button>
+    </Card>
+  )
+}
