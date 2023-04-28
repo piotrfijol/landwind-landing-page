36604fbe6c0028a582714a63b71a30ca349c7859
Piotr Fijol <piotr.fijol1@gmail.com>
2023-04-28 11:22:29 +0200

    Make ListItem accept a bulletpoint prop
    Apart from accepting a bulletpoint prop the ListItem component
    precedes content of li with the custom bulletpoint if the prop was
    provided.

--- src/components/List/ListItem.jsx
+++ src/components/List/ListItem.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
 
-export const ListItem = ({className = "", children}) => {
+export const ListItem = ({bulletpoint, className = "", children}) => {
+
   return (
-    <li className={`${className} mb-4 font-medium text-base`}>{children}</li>
-    )
+    <li 
+      className={`${className} flex gap-x-2.5 py-0.5 mb-4 font-medium text-base leading-normal`}>
+       {bulletpoint ?
+        <img src={bulletpoint} aria-hidden="true" />
+        : null}
+        {children}
+    </li>
+  )
 }
