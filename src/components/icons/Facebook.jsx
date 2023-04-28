b8e3aad66d349cb338429db3df3406395389e13f
Piotr Fijol <piotr.fijol1@gmail.com>
2023-04-28 18:42:00 +0200

    Add icons for the footer
    Since flowbite-react's Footer.Icon component expects icon prop
    to be passed an svg as a value I chose to create components for each
    icon to reduce the size of Footer's source code.

---
+++ src/components/icons/Facebook.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Facebook = () => {
+  return (
+    <svg width="10" height="18" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="m9.04 10.13.49-3.24H6.4V4.8c0-.92.42-1.77 1.83-1.77h1.44V.25S8.37 0 7.14 0C4.57 0 2.88 1.58 2.88 4.4v2.5H0v3.22h2.88V18H6.4v-7.88h2.64Z" fill="#9CA3AF"/>
+    </svg>
+  )
+}
